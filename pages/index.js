defcdcf1fa4ba2a07f1684c2820c6b63e2458c05
AiInvestorA <ainvestorai@gmail.com>
2025-07-09 21:37:03 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white text-black font-sans p-10">
+      <h1 className="text-4xl font-bold text-center">
+        Welcome to <span className="text-green-600">InvestAi</span>
+      </h1>
+      <p className="mt-4 text-center">This is your live prototype site.</p>
+    </main>
+  );
+}
